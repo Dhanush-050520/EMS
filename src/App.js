48bc7a704eba3e622374bd305f71c4e01db53efc
rohinibyku <rohinibyku7729@gmail.com>
2023-07-09 08:20:35 +0530

    modified:   db.json         modified:   package-lock.json         modified:   package.json         modified:   src/App.js         modified:   src/Login.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Login from './Login'
+import Dashboard from './Dashboard';
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+         <Route path ='dashboard' element={<Dashboard/>}/>
+
+      
+      </Routes>
+      </BrowserRouter>
       <Login/>  
   
  
--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import {Button,Form,Card} from 'react-bootstrap'
 import './App.css'
 import { FaUserTie } from 'react-icons/fa';
+import  { useNavigate } from 'react-router-dom';
 
 
 
 
 
 const Login=()=>{
+
+const navigate=useNavigate();
+
+
+  const [state,setState]=useState({user:' ',pass:' '})
+  const {user,pass}=state;
+  const changeHandler=(e)=>{
+           e.preventDefault();
+           setState({...state,[e.target.name]:e.target.value})
+  }
+  const submitHandler=(e)=>{
+      e.preventDefault();
+       fetch('http://localhost:5000/users').then((res)=>res.json()).then((res2)=>{
+        const usercred=res2.find((ele)=>{ return ele.username===user && ele.password===pass})
+        if(usercred){
+             console.log("login success")
+        }
+        else{
+             console.log("login failure")
+        }
+       })
+  }
+
+
+  
     return(
        <div>
          
           
         <center>
        <Card style={{width:"20rem" ,marginTop:"10rem",padding:"20px",backgroundColor:"skyblue",borderRadius:"20px"}}>
-  <Form>
+  <Form  onSubmit={submitHandler}>
   <Form.Group controlId="formBasicEmail">
     <h1>Login</h1>
     <FaUserTie size="3rem"/>
     <h5 style={{marginTop:"3px"}}>Sign in to your account</h5><br/>
     <Form.Label>Email address</Form.Label>
-    <Form.Control  type="email" placeholder="Username" style={{borderRadius:"7px"}}/>
+    <Form.Control  type="email" placeholder="Username"  name ='user' value ={user} onChange={(e)=>changeHandler(e)} style={{borderRadius:"7px"}}/>
     <Form.Text className="text-muted">
       
     </Form.Text>
@@ -28,9 +54,9 @@ const Login=()=>{
   <Form.Group controlId="formBasicPassword">
     <Form.Label >Password</Form.Label>
    
-    <Form.Control type="password" placeholder="Password" style={{borderRadius:"7px"}}/>
+    <Form.Control type="password" placeholder="Password" name="pass" value={pass} onChange={(e)=>changeHandler(e)} style={{borderRadius:"7px"}}/>
   </Form.Group><br/>
-  <Button variant="primary" type="submit" className='logdesign'>
+  <Button type="submit"  onClick={()=>navigate("dashboard")}variant="primary"  className='logdesign'>
     Login
   </Button>
 </Form>
